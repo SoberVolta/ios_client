9b0a70f8ff931f3661de8b566c3a90cd7325ccab
grbcp5 <grbcp5@mst.edu>
2018-01-02 19:22:56 -0600

    Add EventDetailViewController shell

---
+++ DedeRides/EventDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  EventDetailViewController.swift
+//  DedeRides
+//
+//  Created by Grant Broadwater on 1/2/18.
+//  Copyright © 2018 Grant Broadwater. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventDetailViewController : UIViewController {
+    
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var eventLocationLabel: UILabel!
+    @IBOutlet weak var deleteBtn: UIButton!
+    
+    @IBAction func requestRideBtnPressed() {
+    }
+    
+    @IBAction func offerRideBtnPressed() {
+    }
+    
+    @IBAction func deleteEventBtnPressed() {
+    }
+    
+}
